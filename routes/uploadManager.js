08780f55a51819e7f8b79cd6011f98a66560a0d4
Arvind Ravulavaru <arvind.ravulavaru@gmail.com>
2015-05-03 17:17:55 +0530

    bug fixes

--- routes/uploadManager.js
+++ routes/uploadManager.js
@@ -1,19 +1,19 @@
 // config the uploader
 var options = {
-    tmpDir:  __dirname + '/../public/uploaded/tmp',
+    tmpDir: __dirname + '/../public/uploaded/tmp',
     publicDir: __dirname + '/../public/uploaded',
     uploadDir: __dirname + '/../public/uploaded/files',
-    uploadUrl:  '/uploaded/files/',
+    uploadUrl: '/uploaded/files/',
     maxPostSize: 11000000000, // 11 GB
-    minFileSize:  1,
-    maxFileSize:  10000000000, // 10 GB
-    acceptFileTypes:  /.+/i,
+    minFileSize: 1,
+    maxFileSize: 10000000000, // 10 GB
+    acceptFileTypes: /.+/i,
     // Files not matched by this regular expression force a download dialog,
     // to prevent executing any scripts in the context of the service domain:
-    inlineFileTypes:  /\.(gif|jpe?g|png)$/i,
-    imageTypes:  /\.(gif|jpe?g|png)$/i,
+    inlineFileTypes: /\.(gif|jpe?g|png)$/i,
+    imageTypes: /\.(gif|jpe?g|png)$/i,
     imageVersions: {
-        maxWidth:  80,
+        maxWidth: 80,
         maxHeight: 80
     },
     accessControl: {
@@ -21,34 +21,37 @@ var options = {
         allowMethods: 'OPTIONS, HEAD, GET, POST, PUT, DELETE',
         allowHeaders: 'Content-Type, Content-Range, Content-Disposition'
     },
+    storage: {
+        type: 'local'
+    },
     nodeStatic: {
-        cache:  3600 // seconds to cache served files
+        cache: 3600 // seconds to cache served files
     }
 };
 
 var uploader = require('blueimp-file-upload-expressjs')(options);
 
 
-module.exports = function (router) {
+module.exports = function(router) {
     router.get('/upload', function(req, res) {
-      uploader.get(req, res, function (obj) {
-            res.send(JSON.stringify(obj)); 
-      });
-      
+        uploader.get(req, res, function(err, obj) {
+            res.send(JSON.stringify(obj));
+        });
+
     });
 
     router.post('/upload', function(req, res) {
-      uploader.post(req, res, function (obj) {
-            res.send(JSON.stringify(obj)); 
-      });
-      
+        uploader.post(req, res, function(err, obj) {
+            res.send(JSON.stringify(obj));
+        });
+
     });
 
     router.delete('/uploaded/files/:name', function(req, res) {
-      uploader.delete(req, res, function (obj) {
-            res.send(JSON.stringify(obj)); 
-      });
-      
+        uploader.delete(req, res, function(err, obj) {
+            res.send(JSON.stringify(obj));
+        });
+
     });
     return router;
 }
